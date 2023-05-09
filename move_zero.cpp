764dc130641ce09567f12c4e7afe21da4b6f8102
Khaisa Shafa Dwinanda <114421749+Khaisa-Shafa@users.noreply.github.com>
2023-05-09 21:20:28 +0700

    no.3

--- move_zero.cpp
+++ move_zero.cpp
@@ -1,40 +1,43 @@
-#include<stdio.h>
 #include<iostream>
-
 using namespace std;
-void update1And0Positions(int arr[], int n) {
-   int index = 0;
-   for (int i = 0; i < n; i++) {
-      if (arr[i] != 1) {
-         arr[index++] = arr[i];
-      }
-   }
-   while (index < n) {
-      arr[index++] = 1;
-   }
-   index = 0;
-   for (int i = n - 1; i >= 0; i--) {
-      if (arr[i] == 1) {
-         continue;
-      }
-       if (!index) {
-         index = i;
-      }
-      if (arr[i] != 0) {
-         arr[index--] = arr[i];
-      }
-   }
-   while (index >= 0) {
-      arr[index--] = 0;
-   }
+
+
+int move_zero(int arr[], int n) {
+
+  int angka = n - 1;
+  int hitung = n - 1;
+
+  while(hitung >= 0) {
+    if(arr[hitung] != 0) {
+      arr[angka] = arr[hitung];
+      angka--;
+    }
+
+    hitung--;
+  }
+
+  while((angka) >= 0) {
+    arr[angka] = 0;
+    angka--;
+  }
+   return angka;
 }
 int main() {
-   int arr[] = {1, 2, 3, 0, 4, 5, 0, 0};
-   int n = sizeof(arr)/sizeof(arr[0]);
-   update1And0Positions(arr, n);
-   for (int i = 0; i < n; i++) {
-      cout << arr[i] << " ";
-   }
-    cout << endl;
-   return 0;
+  int arr[] = {1, 2, 3, 0, 4, 5, 0, 0};
+  int n = sizeof(arr) / sizeof(arr[0]);
+
+  cout << "Sebelum : ";
+  
+  for(int i = 0 ; i<n; i++) {
+    cout << arr[i] << " ";
+    
+  }  
+  
+  move_zero(arr, n);
+  
+  cout << endl<< "Sesudah : ";
+  for(int i = 0 ; i < n; i++) {
+    cout << arr[i] << " ";
+    
+} 
 }
